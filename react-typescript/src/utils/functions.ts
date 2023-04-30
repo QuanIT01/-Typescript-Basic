871b73854d04076720b6fa45c3ced84217e7680e
QuanIT01 <n.h.quan126@gmail.com>
2023-04-30 08:03:45 +0700

    Function types additonal part1

--- react-typescript/src/utils/functions.ts
+++ react-typescript/src/utils/functions.ts
@@ -38,3 +38,41 @@ function parseCoodinate(arg1: any, arg2?: any): Coodinate {
   }
   return coord;
 }
+// Normal function
+function addNumber(a: number, b: number): number {
+  return a + b;
+}
+// arrow parameters
+const addStrings = (x: string, y: string): string => {
+  return `${x}${y}`;
+};
+// default parameters
+function addNumbersWithDefaultPPArams(a: number = 10, b: number = 20): number {
+  return a + b;
+}
+// union types
+function format(
+  title: string,
+  description: string,
+  amount: string | number
+): string {
+  return `${title} ${description} ${amount}`;
+}
+format("evondev", "developer", 50);
+format("evondev", "developer", "50s");
+// void functions
+function contact(email: string, phone: number) {
+  console.log(email, phone);
+}
+// promise functions
+const fetchData = (url: string): Promise<string> =>
+  Promise.resolve(`Get data from ${url}`);
+//rest parameters
+function information(id: number, ...names: string[]) {
+  return `${id} ${names.join(" ")}`;
+}
+information(1, "evondev", "tuan");
+// with callback
+//params with params
+// function ass types
+//function return function
