214a001bb193a9d7e5a8a48f672407c6a1dca629
QuanIT01 <n.h.quan126@gmail.com>
2023-04-30 07:47:45 +0700

    Intersection and assertions and casting types

--- react-typescript/src/components/Card.tsx
+++ react-typescript/src/components/Card.tsx
@@ -1,6 +1,14 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { ICard } from "../utils/interface";
 
 const Card = ({ title, description, image, link }: ICard) => {
-  return <div></div>;
+  useEffect(() => {
+    const input = document.querySelector("input") as HTMLInputElement;
+    console.log(input.value);
+  }, []);
+  return (
+    <div>
+      <input type="text" />
+    </div>
+  );
 };
--- react-typescript/src/utils/types.ts
+++ react-typescript/src/utils/types.ts
@@ -1,1 +1,40 @@
 export type Age = 18 | 30 | 40;
+//union type typeA | typeB
+//Intersection types: typesA & typesB & typesC
+interface IBusinessPartne {
+  name: string;
+  credit: number;
+}
+interface IIdentity {
+  id: number;
+  name: string;
+}
+interface IContact {
+  email: string;
+  phone: string;
+}
+
+type Employee = IIdentity & IContact;
+type Customer = IBusinessPartne & IContact;
+type Other = IContact | IIdentity;
+
+let evondev: Employee = {
+  id: 1,
+  name: "evondev",
+  email: "evondev@gmail.com",
+  phone: "123456",
+};
+let Quan: Customer = {
+  credit: 12,
+  name: "quan",
+  email: "quan@gmail.com",
+  phone: "123456",
+};
+
+let other: Other = {
+  email: "abc@gmail.com",
+  phone: "01236",
+};
+
+// assertions types as
+//casting types as - mô tả  ở bên card.ts
