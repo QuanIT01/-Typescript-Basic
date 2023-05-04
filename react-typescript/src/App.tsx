9abd9ff4800f08cdeea3068d8f28e13368015593
QuanIT01 <n.h.quan126@gmail.com>
2023-05-04 22:36:58 +0700

    typescript with  useState and  useEffect and  children

--- react-typescript/src/App.tsx
+++ react-typescript/src/App.tsx
@@ -1,7 +1,7 @@
-import React, { useReducer, useRef } from "react";
+import React, { useEffect, useReducer, useRef, useState } from "react";
 
 const Heading = ({ title }: { title: string }) => {
-  return <h2 className="font-primary font-bold text-2xl mb-5">{title}</h2>;
+  return <h2 className="mb-5 text-2xl font-bold font-primary">{title}</h2>;
 };
 type ActionType =
   | { type: "ADD"; text: string }
@@ -27,6 +27,9 @@ const todoReducer = (state: Todo[], action: ActionType) => {
   }
 };
 const initialState: Todo[] = [];
+interface Data {
+  text: string;
+}
 
 const App = () => {
   const [todos, dispatch] = useReducer(todoReducer, initialState);
@@ -46,17 +49,37 @@ const App = () => {
       inputRef.current.value = "";
     }
   };
+  //
+  const [data, setData] = useState<Data | null>(null);
+  useEffect(() => {
+    fetch("/data.json")
+      .then((res) => res.json())
+      .then((result: Data) => {
+        setData(result);
+      });
+  }, []);
+  const onClickItem = (item: string) => {
+    alert(item);
+  };
   return (
     <div>
       <Heading title="Todo App"></Heading>
+      {JSON.stringify(data)}
+      <List
+        items={["javascript", "html", "css"]}
+        onClickItem={(item: string) => onClickItem(item)}
+      ></List>
+      <Boxed>
+        <div>abc</div>
+      </Boxed>
       <div className="max-w-sm">
-        <div className="mb-5 flex flex-col gap-y-5">
+        <div className="flex flex-col mb-5 gap-y-5">
           {todos.map((todo) => (
             <div className="flex items-center gap-x-3" key={todo.id}>
               <span>{todo.text}s</span>
               <button
                 onClick={() => OnRemoveTodo(todo.id)}
-                className="p-2 rounded-lg bg-red-500 text-white font-medium text-sm"
+                className="p-2 text-sm font-medium text-white bg-red-500 rounded-lg"
               >
                 Remove
               </button>
@@ -66,12 +89,12 @@ const App = () => {
         <div className="flex items-center gap-x-5">
           <input
             type="text"
-            className="p-4 border border-slate-200 rounded-lg outline-nones"
+            className="p-4 border rounded-lg border-slate-200 outline-nones"
             ref={inputRef}
           />
           <button
             onClick={onAddTodo}
-            className="p-4 bg-blue-500 rounded-lg text-white text-center"
+            className="p-4 text-center text-white bg-blue-500 rounded-lg"
           >
             Add todo
           </button>
@@ -79,6 +102,28 @@ const App = () => {
       </div>
     </div>
   );
+};
+
+const List = ({
+  items,
+  onClickItem = () => {},
+}: {
+  items: string[];
+  onClickItem?: (item: string) => void;
+}) => {
+  return (
+    <div>
+      {items.map((item) => (
+        <div key={item} onClick={() => onClickItem?.(item)}>
+          {item}
+        </div>
+      ))}
+    </div>
+  );
+};
+
+const Boxed = ({ children }: { children: React.ReactNode }) => {
+  return <div>{children}</div>;
 };
 
 export default App;
